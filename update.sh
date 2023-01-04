e47c5805b9aa80708b11f758a4c369ed8311bcb4
distek <distek@protonmail.com>
2023-01-04 06:06:07 -0800

    update

--- update.sh
+++ update.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 cat ~/.vscode/extensions/extensions.json | jq '.[].identifier.id' >extensions
+cp ~/.vscode/custom.css .
+cp ~/.vscode/indicator.js .
 
 cp ~/.config/Code/User/keybindings.json .
 cp ~/.config/Code/User/settings.json .
 
-git stage keybindings.json settings.json extensions
+sed -i "s#$HOME#%HOMEPATH%#g" settings.json
+
+git stage keybindings.json settings.json extensions custom.css indicator.js
 
 git commit -m "update"
 
